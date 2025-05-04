66d00d3410c8370d3f357c7aaebd7520478e6497
melvins simon <melvinssimon@gmail.com>
2025-05-04 20:05:00 +0300

    face-api-bugs-fixed

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,25 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  define: {
+    global: "globalThis",
+    "process.env": {},
+    "window.global": "window",
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: "globalThis",
+      },
+      plugins: [
+        (
+          await import("@esbuild-plugins/node-globals-polyfill")
+        ).NodeGlobalsPolyfillPlugin({
+          buffer: true,
+        }),
+      ],
+    },
+  },
 });
