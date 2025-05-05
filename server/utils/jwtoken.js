3321bf345156a8e7fd7be8d757d792f0b075fd45
melvins simon <melvinssimon@gmail.com>
2025-05-05 09:01:41 +0300

    fixing bugs

--- server/utils/jwtoken.js
+++ server/utils/jwtoken.js
@@ -2,18 +2,18 @@ import jwt from "jsonwebtoken";
 import "dotenv/config";
 
 export const generateJwtAsetCookie = (userId, res) => {
-  try {
-    const token = jwt.sign({ userId }, process.env.SECRETE_KEY, {
-      expiresIn: "7d",
-    });
-    res.cookie("authorization", token, {
-      httpOnly: true,
-      sameSite: "strict",
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-      secure: process.env.NODE_ENV === "production",
-    });
-    return token;
-  } catch (error) {
-    throw new Error(error);
-  }
+  const token = jwt.sign({ userId }, process.env.SECRETE_KEY, {
+    expiresIn: "7d",
+  });
+
+  res.cookie("authorization", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    domain: "emarker-face-recognition.vercel.app",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    path: "/",
+  });
+
+  return token;
 };
